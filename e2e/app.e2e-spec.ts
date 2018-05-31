74fcd847b4437ab8eca40403cb0de8867b725b4e
Angular CLI <angular-cli@angular.io>
2018-05-30 21:04:11 -0700

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('scla-blog-front App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
